4c03ce1e05862d444ddb1103555934c152f124f3
“growth-ai-labs” <growth.ai.labs@gmail.com>
2025-12-30 16:35:36 -0500

    Edit message from MacBook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Python environment is set up correctly 🚀")
+    print("Python environment is set up correctly 🚀 (edited from MacBook)")
 
 if __name__ == "__main__":
     main()
